c6400ec564c3639198cb881bf740dd8d30ddcf7d
Matias Meno <m@tias.me>
2014-06-25 16:49:50 -0700

    Added logging

--- bin/start_server.dart
+++ bin/start_server.dart
@@ -1,11 +1,16 @@
 import '../lib/services/services.dart';
-
+import 'package:logging/logging.dart';
 
 /**
  * Gets the configured [Iris] instance with [getIris]
  * and starts the servers with it.
  */
 void main() {
+
+  // Configure the logger to simply print all messages
+  Logger.root.onRecord.listen((LogRecord record) => print(record.message));
+
+
   getIris().startServers()
       .then((_) => print('Server started successfully'))
       .catchError((err) => print('Oups, something went wrong: $err'));
